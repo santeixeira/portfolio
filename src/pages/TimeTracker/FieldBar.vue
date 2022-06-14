18acf69b9aadcdcebbe1fd3fb09a0a2566c43e54
santeixeira <jbsanthiago@gmail.com>
2022-06-14 19:01:15 -0300

    Hotfix

--- src/pages/TimeTracker/FieldBar.vue
+++ src/pages/TimeTracker/FieldBar.vue
@@ -1,17 +1,24 @@
 <template>
   <ListBox>
     <div class="columns">
-      <div class="column is-8" role="form" aria-label="Form for new task">
-        <input type="text" class="input" placeholder="Nomeia a nova tarefa" />
+      <div class="column is-5" role="form" aria-label="Form for new task">
+        <input
+          type="text"
+          class="input"
+          placeholder="Nomeia a nova tarefa"
+          v-model="description"
+        />
+      </div>
+      <div class="column is-3">
+        <div class="select is-primary">
+          <select>
+            <option>Select dropdown</option>
+            <option>With options</option>
+          </select>
+        </div>
       </div>
       <div class="column is-4 organize-list">
-        <Cronometer :timeSeconds="timeSeconds" />
-        <button class="button" @click="startCount">
-          <span class="icon">
-            <i :class="icon"></i>
-          </span>
-          <span>{{ text }}</span>
-        </button>
+        <Temporizer @endedTime="end" />
       </div>
     </div>
   </ListBox>
@@ -19,35 +26,19 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { ListBox, ListContent, Cronometer } from "@/components/index";
+import { ListBox } from "@/components/index";
+import { Temporizer } from "./index";
 export default defineComponent({
   name: "FieldBar",
-  components: { ListBox, Cronometer },
-
-  data() {
+  components: { ListBox, Temporizer },
+  data () {
     return {
-      timeSeconds: 0,
-      cronometer: 0,
-      timeCondition: true,
-      icon: "fas fa-play",
-      text: "play"
-    };
+      description: "",
+    }
   },
   methods: {
-    startCount() {
-      if (this.timeCondition == true) {
-        this.cronometer = setInterval(() => {
-          this.timeSeconds++;
-        }, 1000);
-        this.timeCondition = false;
-        this.icon = "fas fa-stop";
-        this.text = "stop";
-      } else {
-        clearInterval(this.cronometer);
-        this.icon = "fas fa-play";
-        this.text = "play";
-        this.timeCondition = true;
-      }
+    end(timePassed: number): void {
+      console.log(timePassed);
     }
   }
 });
